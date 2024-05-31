cd75e98e1fe0b5bfc97fb19450673b01477f2cda
EducationalLight69420 <142517618+EducationalLight69420@users.noreply.github.com>
2024-05-31 06:52:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import razorpay
 from pyscript import display
 
-display("Hello World")
+client = razorpay.Client(auth=("rzp_live_h7HHdgtBmhiyff", "yaVQNulYrZwNoetoMPhGySKR"))
+
+DATA = {
+    "amount": 100,
+    "currency": "INR",
+    "receipt": "receipt#1",
+    "notes": {
+        "key1": "value3",
+        "key2": "value2"
+    }
+}
+x = client.order.create(data=DATA)
+print(x['id'])
